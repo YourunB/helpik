8022951f03b78e9c5d850f9e6e3fb6a77c61f991
yourunb <bxyura@gmail.com>
2025-01-12 13:49:48 +0300

    refactor: complete optimiztion by indexOf search

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const getAnswer = () => {
 	let result = "";
 	
 	for (let i = 0; i < data.length; i++) {
-		if (data[i].question.toLowerCase().includes(chars)) {
+		if (data[i].question.toLowerCase().indexOf(chars) !== -1) {
 			result += `\nВопрос: ${data[i].question}\nОтвет: ${data[i].answer}\n`;
 		}
 	}
